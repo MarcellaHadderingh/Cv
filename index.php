d0adb9c73b82ea28fe9c9a3ea47c043be56b287e
marcellaHadderingh <marcellavanduyn@gmail.com>
2019-01-07 10:47:20 +0100

    added progressbar
    added progressbar SQL and changed progress bars PHP,Bootstrap, Javascript.

--- index.php
+++ index.php
@@ -244,7 +244,7 @@
                         <li>
                             <div class="progress">
                                 <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:77%">
-                                    77%
+                                    75%
                                 </div>
                             </div>
                         </li>
@@ -259,24 +259,32 @@
                         <h4><i class="fab fa-js-square"></i> Javascript</h4>
                         <li>
                             <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:10%">
-                                    10%
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:20%">
+                                    20%
                                 </div>
                             </div>
                         </li>
                         <h4><i class="fas fa-file-code"></i> Bootstrap</h4>
                         <li>
                             <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:70%">
-                                    70%
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:77%">
+                                    77%
                                 </div>
                             </div>
                         </li>
                         <h4><i class="fab fa-php"></i> PHP</h4>
                         <li>
                             <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:10%">
-                                    10%
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:14%">
+                                    14%
+                                </div>
+                            </div>
+                        </li>
+                        <h4>SQL</h4>
+                        <li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:0%">
+                                    0%
                                 </div>
                             </div>
                         </li>
